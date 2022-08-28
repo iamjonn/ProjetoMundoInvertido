c959dddbd656bf2dce887639d328682168eb599b
jon <110827553+iamjonn@users.noreply.github.com>
2022-08-27 23:37:06 -0300

    Update firebase.js

--- assets/js/data/firebase.js
+++ assets/js/data/firebase.js
@@ -1,11 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-import firebaseConfig from './config.js'
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC_Uzq4ktzzlVFJQXLqwFRlVL_z9ed9z3E",
+    authDomain: "jon-stranger.firebaseapp.com",
+    projectId: "jon-stranger",
+    storageBucket: "jon-stranger.appspot.com",
+    messagingSenderId: "1085196343043",
+    appId: "1:1085196343043:web:fe69a89eebdcc4cf46bb36"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 export default app
